05c72724bdecc0800c6781c3dcbef9cb263a55af
scinttt <scinttt@gmail.com>
2025-05-27 21:22:37 -0400

    Initialization

---
+++ src/main/java/com/EphAI/Eph_Code/EphCodeApplication.java
@@ -0,0 +1,13 @@
+package com.EphAI.Eph_Code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EphCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EphCodeApplication.class, args);
+	}
+
+}
